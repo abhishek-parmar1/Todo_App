45e82dd76cdce7070982a1f121cfd2737a269a63
Abhishek <parmarabhishek.53@gmail.com>
2017-07-24 23:21:51 +0530

    browser storage used to store dynamic todo

--- assets/custom/js/custom.js
+++ assets/custom/js/custom.js
@@ -1,35 +1,23 @@
-var ngTodo = angular.module('ngTodo', ['ngAnimate']);
+var ngTodo = angular.module('ngTodo', ['ngStorage']);
 
-ngTodo.controller('mainController',function($scope){
+ngTodo.controller('mainController',function($scope,$localStorage){
     
-    $scope.todoArray = [
-        {
-            title : 'Get-up',
-            details : 'I must get-up early today.'
-        },
-        {
-            title : 'Attend Class',
-            details : 'I must Attend Class today.'
-        },
-        {
-            title : 'Download Lecture',
-            details : 'I must Download Lecture today.'
-        },
-        {
-            title : 'Complete Extra Project Objectives',
-            details : 'I must Complete Extra Project Objectives today.'
-        },
-        {
-            title : 'Work on Self Project',
-            details : 'I must Work on Self Project today.'
+    $scope.todoArray = [];
+    
+    if($localStorage.todoArray == undefined){
+            $localStorage.todoArray = $scope.todoArray;
         }
-                        ];
+    else{
+        $scope.todoArray = $localStorage.todoArray;
+    }
     
     $scope.addTodo = function(newTodo){
         $scope.todoArray.splice(0,0, { title : newTodo.name, details : newTodo.details});
+        $localStorage.todoArray = $scope.todoArray;
     }
     
     $scope.removeTodo = function(index){
         $scope.todoArray.splice(index,1);
+        $localStorage.todoArray = $scope.todoArray;
     }
 });
